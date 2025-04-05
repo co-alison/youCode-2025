3ad63a0084bc210ae6e58128e3413a299b71650a
Cindy Cui <misscindycui@gmail.com>
2025-04-05 12:02:46 -0700

    Initial Commit

---
+++ youCode2025/youCode2025App.swift
@@ -0,0 +1,17 @@
+//
+//  youCode2025App.swift
+//  youCode2025
+//
+//  Created by Cindy Cui on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct youCode2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
