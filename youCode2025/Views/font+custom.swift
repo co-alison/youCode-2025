4cd305bf4b33ccf98758b9240e2e052b4831dc3d
anisapirani <apiran03@student.ubc.ca>
2025-04-06 09:57:36 -0700

    made login welcome and signup page v2

---
+++ youCode2025/Views/font+custom.swift
@@ -0,0 +1,14 @@
+//
+//  Font+Custom.swift
+//  youCode2025
+//
+//  Created by Anisa Pirani on 2025-04-06.
+//
+
+import SwiftUI
+
+extension Font {
+    static func elanTitle(size: CGFloat) -> Font {
+        return .custom("ElanITCStd-Medium", size: size)
+    }
+}
